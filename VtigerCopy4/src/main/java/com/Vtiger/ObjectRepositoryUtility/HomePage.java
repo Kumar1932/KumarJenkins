1d12512c6d91aeb1b8b88ee6a55566a86a57c624
kumar <kumar@Kumarswamy-PC>
2025-03-06 11:00:26 +0530

    hi

--- VtigerCopy4/src/main/java/com/Vtiger/ObjectRepositoryUtility/HomePage.java
+++ VtigerCopy4/src/main/java/com/Vtiger/ObjectRepositoryUtility/HomePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import com.Vtiger.BaseUtility.BaseClass;
 import com.Vtiger.generic.WebDriverUtility.WebDriverUtility;
 
 /**
@@ -12,10 +13,9 @@ import com.Vtiger.generic.WebDriverUtility.WebDriverUtility;
  * @author kumar
  */
 
-public class HomePage 
+public class HomePage extends BaseClass
 {
 	WebDriverUtility wlib=new WebDriverUtility();
-   
 	public HomePage(WebDriver driver) 
 	{
 	 PageFactory.initElements(driver, this);
